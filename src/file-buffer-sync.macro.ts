6b7cf30790b4c314cea035f2534f03de07381b15
Beeno Tung <aabbcc1241@yahoo.com.hk>
2023-07-08 18:01:04 +0800

    chore: reuse type_sizes instead of inline in each macro source file

--- src/file-buffer-sync.macro.ts
+++ src/file-buffer-sync.macro.ts
@@ -1,24 +1,6 @@
+import { type_sizes } from "./size"
+
 function gen() {
-  const type_sizes = {
-    'Int8': 1,
-    'Int16BE': 2,
-    'Int16LE': 2,
-    'Int32BE': 4,
-    'Int32LE': 4,
-    'BigInt64BE': 8,
-    'BigInt64LE': 8,
-    'UInt8': 1,
-    'UInt16BE': 2,
-    'UInt16LE': 2,
-    'UInt32BE': 4,
-    'UInt32LE': 4,
-    'BigUInt64BE': 8,
-    'BigUInt64LE': 8,
-    'FloatBE': 4,
-    'FloatLE': 4,
-    'DoubleBE': 8,
-    'DoubleLE': 8,
-  }
   return `
 import fs from 'fs'
 import { ensureEnoughRead, ensureEnoughWrite, expandArguments, writeBufferNT } from './utils'
